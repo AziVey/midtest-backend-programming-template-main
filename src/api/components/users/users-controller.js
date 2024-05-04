dae817a9d1930fc426c6937037315c2317d6d445
AziVey <cytrsx01@gmail.com>
2024-05-04 15:06:48 +0700

    second commit

---
+++ src/api/components/users/users-controller.js
@@ -0,0 +1,246 @@
+const usersService = require('./users-service');
+const { errorResponder, errorTypes } = require('../../../core/errors');
+
+/**
+ * Handle get list of users request
+ * @param {object} request - Express request object
+ * @param {object} response - Express response object
+ * @param {object} next - Express route middlewares
+ * @returns {object} Response object or pass an error to the next route
+ */
+async function getUsers(request, response, next) {
+  try {
+    let users = await usersService.getUsers();
+
+    if (request.query.sort) {
+      const sortField = request.query.sort.split(':')[0];
+      const sortOrder = request.query.sort.split(':')[1];
+
+      users = users.sort((a, b) => {
+        if (sortOrder === 'asc') {
+          return a[sortField] > b[sortField] ? 1 : -1;
+        } else {
+          return a[sortField] < b[sortField] ? 1 : -1;
+        }
+      });
+    }
+
+    if (request.query.search) {
+      const searchQuery = request.query.search.split('=');
+      const searchField = searchQuery[0];
+      const searchTerm = searchQuery[1];
+
+      const regex = new RegExp(searchTerm, 'i');
+
+      users = users.filter((user) => {
+        return regex.test(user[searchField]);
+      });
+    }
+
+    const page = parseInt(request.query.page) || 1;
+    const limit = parseInt(request.query.limit) || 5;
+
+    const startIndex = (page - 1) * limit;
+    const endIndex = page * limit;
+
+    const totalUsers = users.length;
+    const totalPages = Math.ceil(totalUsers / limit);
+
+    // Paginate the users
+    const paginatedUsers = users.slice(startIndex, endIndex);
+
+    const result = {
+      page_number: page,
+      page_size: limit,
+      count: paginatedUsers.length,
+      total_pages: totalPages,
+      has_previous_page: page > 1,
+      has_next_page: endIndex < totalUsers,
+      data: paginatedUsers,
+    };
+    return response.status(200).json(users);
+  } catch (error) {
+    return next(error);
+  }
+}
+
+/**
+ * Handle get user detail request
+ * @param {object} request - Express request object
+ * @param {object} response - Express response object
+ * @param {object} next - Express route middlewares
+ * @returns {object} Response object or pass an error to the next route
+ */
+async function getUser(request, response, next) {
+  try {
+    const user = await usersService.getUser(request.params.id);
+
+    if (!user) {
+      throw errorResponder(errorTypes.UNPROCESSABLE_ENTITY, 'Unknown user');
+    }
+
+    return response.status(200).json(user);
+  } catch (error) {
+    return next(error);
+  }
+}
+
+/**
+ * Handle create user request
+ * @param {object} request - Express request object
+ * @param {object} response - Express response object
+ * @param {object} next - Express route middlewares
+ * @returns {object} Response object or pass an error to the next route
+ */
+async function createUser(request, response, next) {
+  try {
+    const name = request.body.name;
+    const email = request.body.email;
+    const password = request.body.password;
+    const password_confirm = request.body.password_confirm;
+
+    // Check confirmation password
+    if (password !== password_confirm) {
+      throw errorResponder(
+        errorTypes.INVALID_PASSWORD,
+        'Password confirmation mismatched'
+      );
+    }
+
+    // Email must be unique
+    const emailIsRegistered = await usersService.emailIsRegistered(email);
+    if (emailIsRegistered) {
+      throw errorResponder(
+        errorTypes.EMAIL_ALREADY_TAKEN,
+        'Email is already registered'
+      );
+    }
+
+    const success = await usersService.createUser(name, email, password);
+    if (!success) {
+      throw errorResponder(
+        errorTypes.UNPROCESSABLE_ENTITY,
+        'Failed to create user'
+      );
+    }
+
+    return response.status(200).json({ name, email });
+  } catch (error) {
+    return next(error);
+  }
+}
+
+/**
+ * Handle update user request
+ * @param {object} request - Express request object
+ * @param {object} response - Express response object
+ * @param {object} next - Express route middlewares
+ * @returns {object} Response object or pass an error to the next route
+ */
+async function updateUser(request, response, next) {
+  try {
+    const id = request.params.id;
+    const name = request.body.name;
+    const email = request.body.email;
+
+    // Email must be unique
+    const emailIsRegistered = await usersService.emailIsRegistered(email);
+    if (emailIsRegistered) {
+      throw errorResponder(
+        errorTypes.EMAIL_ALREADY_TAKEN,
+        'Email is already registered'
+      );
+    }
+
+    const success = await usersService.updateUser(id, name, email);
+    if (!success) {
+      throw errorResponder(
+        errorTypes.UNPROCESSABLE_ENTITY,
+        'Failed to update user'
+      );
+    }
+
+    return response.status(200).json({ id });
+  } catch (error) {
+    return next(error);
+  }
+}
+
+/**
+ * Handle delete user request
+ * @param {object} request - Express request object
+ * @param {object} response - Express response object
+ * @param {object} next - Express route middlewares
+ * @returns {object} Response object or pass an error to the next route
+ */
+async function deleteUser(request, response, next) {
+  try {
+    const id = request.params.id;
+
+    const success = await usersService.deleteUser(id);
+    if (!success) {
+      throw errorResponder(
+        errorTypes.UNPROCESSABLE_ENTITY,
+        'Failed to delete user'
+      );
+    }
+
+    return response.status(200).json({ id });
+  } catch (error) {
+    return next(error);
+  }
+}
+
+/**
+ * Handle change user password request
+ * @param {object} request - Express request object
+ * @param {object} response - Express response object
+ * @param {object} next - Express route middlewares
+ * @returns {object} Response object or pass an error to the next route
+ */
+async function changePassword(request, response, next) {
+  try {
+    // Check password confirmation
+    if (request.body.password_new !== request.body.password_confirm) {
+      throw errorResponder(
+        errorTypes.INVALID_PASSWORD,
+        'Password confirmation mismatched'
+      );
+    }
+
+    // Check old password
+    if (
+      !(await usersService.checkPassword(
+        request.params.id,
+        request.body.password_old
+      ))
+    ) {
+      throw errorResponder(errorTypes.INVALID_CREDENTIALS, 'Wrong password');
+    }
+
+    const changeSuccess = await usersService.changePassword(
+      request.params.id,
+      request.body.password_new
+    );
+
+    if (!changeSuccess) {
+      throw errorResponder(
+        errorTypes.UNPROCESSABLE_ENTITY,
+        'Failed to change password'
+      );
+    }
+
+    return response.status(200).json({ id: request.params.id });
+  } catch (error) {
+    return next(error);
+  }
+}
+
+module.exports = {
+  getUsers,
+  getUser,
+  createUser,
+  updateUser,
+  deleteUser,
+  changePassword,
+};
